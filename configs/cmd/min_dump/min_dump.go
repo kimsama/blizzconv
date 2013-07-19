3295396d71f331f7aea35322cd07de710075afd5
mewmew <rnd0x00@gmail.com>
2013-07-19 14:22:25 +0200

    configs/cmd/min_dump: Update import paths.

--- configs/cmd/min_dump/min_dump.go
+++ configs/cmd/min_dump/min_dump.go
@@ -1,20 +1,38 @@
+// min_dump is a tool for constructing pillars, based on the information
+// retrieved from a given MIN file, and storing these pillars as png images.
+//
+// Usage:
+//
+//    min_dump [OPTION]... [name.min]...
+//
+// Flags:
+//
+//    -celini="cel.ini"
+//            Path to an ini file containing image information.
+//            Note: 'cl2.ini' will be used for files that have the '.cl2' extension.
+//    -mpqdump="mpqdump/"
+//            Path to an extracted MPQ file.
+//    -mpqini="mpq.ini"
+//            Path to an ini file containing relative path information.
 package main
 
-import dbg "fmt"
-import "flag"
-import "fmt"
-import "image"
-import "log"
-import "os"
-import "path"
-import "strings"
+import (
+	"flag"
+	dbg "fmt"
+	"fmt"
+	"image"
+	"log"
+	"os"
+	"path"
+	"strings"
 
-import "github.com/0xC3/progress/barcli"
-import "github.com/mewkiz/pkg/imgutil"
-import "github.com/mewrnd/blizzconv/configs/min"
-import "github.com/mewrnd/blizzconv/images/cel"
-import "github.com/mewrnd/blizzconv/images/imgconf"
-import "github.com/mewrnd/blizzconv/mpq"
+	"github.com/0xC3/progress/barcli"
+	"github.com/mewkiz/pkg/imgutil"
+	"github.com/mewrnd/blizzconv/configs/min"
+	"github.com/mewrnd/blizzconv/images/cel"
+	"github.com/mewrnd/blizzconv/images/imgconf"
+	"github.com/mewrnd/blizzconv/mpq"
+)
 
 func init() {
 	flag.Usage = usage
