fc470b2d5445c921b829bc6d3e2740234d2b3a4e
mewmew <rnd0x00@gmail.com>
2013-07-19 16:11:56 +0200

    images/trn: Initial commit.

---
+++ images/trn/trn.go
@@ -0,0 +1,7 @@
+// Package trn implements functionality for parsing TRN files.
+//
+// TRN files contain information about color transitions for images, similar to
+// how palettes work. Below is a description of the TRN format.
+//
+/// ### todo ###
+package trn
